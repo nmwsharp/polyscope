04beb512a604009e703e617792b6a753a37fd094
Nick Sharp <nsharp@cs.cmu.edu>
2017-11-21 22:46:43 -0500

    nice font

--- src/polyscope.cpp
+++ src/polyscope.cpp
@@ -489,6 +489,23 @@ void init() {
     // Set up ImGUI glfw bindings
     ImGui_ImplGlfwGL3_Init(g_Window, true);
 
+    // Load better font
+    // - If no fonts are loaded, dear imgui will use the default font. You can also load multiple fonts and use ImGui::PushFont()/PopFont() to select them. 
+    // - AddFontFromFileTTF() will return the ImFont* so you can store it if you need to select the font among multiple. 
+    // - If the file cannot be loaded, the function will return NULL. Please handle those errors in your application (e.g. use an assertion, or display an error and quit).
+    // - The fonts will be rasterized at a given size (w/ oversampling) and stored into a texture when calling ImFontAtlas::Build()/GetTexDataAsXXXX(), which ImGui_ImplXXXX_NewFrame below will call.
+    // - Read 'extra_fonts/README.txt' for more instructions and details.
+    // - Remember that in C/C++ if you want to include a backslash \ in a string literal you need to write a double backslash \\ !
+    ImGuiIO& io = ImGui::GetIO();
+    // io.Fonts->AddFontDefault();
+    //io.Fonts->AddFontFromFileTTF("../deps/imgui/imgui/extra_fonts/Roboto-Medium.ttf", 16.0f);
+    io.Fonts->AddFontFromFileTTF("../deps/imgui/imgui/extra_fonts/Cousine-Regular.ttf", 15.0f);
+    //io.Fonts->AddFontFromFileTTF("../deps/imgui/imgui/extra_fonts/DroidSans.ttf", 16.0f);
+    //io.Fonts->AddFontFromFileTTF("../deps/imgui/imgui/extra_fonts/ProggyTiny.ttf", 10.0f);
+    //ImFont* font = io.Fonts->AddFontFromFileTTF("c:\\Windows\\Fonts\\ArialUni.ttf", 18.0f, NULL, io.Fonts->GetGlyphRangesJapanese());
+    //IM_ASSERT(font != NULL);
+
+
     state::initialized = true;
 }
 
