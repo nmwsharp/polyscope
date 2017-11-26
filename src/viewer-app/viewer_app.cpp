e9e0832ec1d91dbf3d928fb5d59c4a3755e3523b
Nick Sharp <nsharp@cs.cmu.edu>
2017-11-26 01:11:36 -0500

    add file parsing to viewer

--- src/viewer-app/viewer_app.cpp
+++ src/viewer-app/viewer_app.cpp
@@ -1,44 +1,115 @@
 #include "polyscope/polyscope.h"
 
-#include "geometrycentral/halfedge_mesh.h"
+#include <iostream>
+
 #include "geometrycentral/geometry.h"
+#include "geometrycentral/halfedge_mesh.h"
 #include "geometrycentral/polygon_soup_mesh.h"
 
+#include "args/args.hxx"
+
 using namespace geometrycentral;
+using std::cerr;
+using std::cout;
+using std::endl;
+using std::string;
+
+// Hack to guess the basename. Certainly does not work in all case,
+// but we can just fall back on returning the full filename.
+std::string guessName(std::string fullname) {
+  size_t startInd = 0;
+  for (std::string sep : {"/", "\\"}) {
+    size_t pos = fullname.rfind(sep);
+    if (pos != std::string::npos) {
+      startInd = std::max(startInd, pos+1);
+    }
+  }
+
+  size_t endInd = fullname.size();
+  for (std::string sep : {"."}) {
+    size_t pos = fullname.rfind(sep);
+    if (pos != std::string::npos) {
+      endInd = std::min(endInd, pos);
+    }
+  }
+
+  if (startInd >= endInd) {
+    return fullname;
+  }
+
+  std::string niceName = fullname.substr(startInd, endInd - startInd);
+  return niceName;
+}
+
+bool endsWith(const std::string& str, const std::string& suffix) {
+  return str.size() >= suffix.size() &&
+         str.compare(str.size() - suffix.size(), suffix.size(), suffix) == 0;
+}
+
+void processFileOBJ(string filename) {
+  // Get a nice name for the file
+  std::string niceName = guessName(filename);
+
+  Geometry<Euclidean>* geom;
+  HalfedgeMesh* mesh = new HalfedgeMesh(PolygonSoupMesh(filename), geom);
+  polyscope::registerSurfaceMesh(niceName, geom);
+
+  delete geom;
+  delete mesh;
+}
+
+void processFile(string filename) {
+  // Dispatch to correct varient
+  if (endsWith(filename, ".obj")) {
+    processFileOBJ(filename);
+  } else {
+    cerr << "Unrecognized file type for " << filename << endl;
+  }
+}
 
 int main(int argc, char** argv) {
-    
+  // Configure the argument parser
+  args::ArgumentParser parser(
+      "A general purpose viewer for geometric data, built on Polyscope.\nBy "
+      "Nick Sharp (nsharp@cs.cmu.edu)",
+      "");
+  args::PositionalList<string> files(parser, "files",
+                                     "One or more files to visualize");
 
-    // Initialize polyscope
-    polyscope::init();
-
-    // Create a point cloud
-    std::vector<Vector3> points;
-    for(size_t i = 0; i < 3000; i++) {
-    // for(size_t i = 0; i < 1; i++) {
-        // points.push_back(Vector3{10,10,10} + 20*Vector3{unitRand()-.5, unitRand()-.5, unitRand()-.5});
-        points.push_back(3*Vector3{unitRand()-.5, unitRand()-.5, unitRand()-.5});
-    }
-
-    // Load the point cloud in to polyscope
-    polyscope::registerPointCloud("really great points", points);
-
-    // Read a mesh
-    Geometry<Euclidean>* geom1;
-    Geometry<Euclidean>* geom2;
-    HalfedgeMesh* mesh1 = new HalfedgeMesh(PolygonSoupMesh("/Users/nsharp/mesh/sphere_medium.obj"), geom1);
-    HalfedgeMesh* mesh2 = new HalfedgeMesh(PolygonSoupMesh("/Users/nsharp/mesh/armadillo.obj"), geom2);
-    // HalfedgeMesh* mesh = new HalfedgeMesh(PolygonSoupMesh("/Users/nsharp/mesh/raptor_orig.obj"), geom);
-    polyscope::registerSurfaceMesh("mr mesh1", geom1);
-    polyscope::registerSurfaceMesh("mr mesh2", geom2);
-
-    // Load the mesh in to polyscope
-
-    // Add a few gui elements
-
-    // Show the gui
-    polyscope::show();
-
+  // Parse args
+  try {
+    parser.ParseCLI(argc, argv);
+  } catch (args::Help) {
+    std::cout << parser;
     return 0;
+  } catch (args::ParseError e) {
+    std::cerr << e.what() << std::endl;
 
+    std::cerr << parser;
+    return 1;
+  }
+
+  // Initialize polyscope
+  polyscope::init();
+
+  for (std::string s : files) {
+    processFile(s);
+  }
+
+  // Create a point cloud
+  std::vector<Vector3> points;
+  for (size_t i = 0; i < 3000; i++) {
+    // points.push_back(Vector3{10,10,10} + 20*Vector3{unitRand()-.5,
+    // unitRand()-.5, unitRand()-.5});
+    points.push_back(
+        3 * Vector3{unitRand() - .5, unitRand() - .5, unitRand() - .5});
+  }
+  polyscope::registerPointCloud("really great points", points);
+
+  // Add a few gui elements
+
+  // Show the gui
+  polyscope::show();
+
+  return 0;
 }
