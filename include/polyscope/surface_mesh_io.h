aed3c7cd9b452d02cbd3ac6067258d0a10fc20d1
Nick Sharp <nsharp@cs.cmu.edu>
2018-07-16 13:08:57 -0400

    add basic mesh io

---
+++ include/polyscope/surface_mesh_io.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "polyscope/utilities.h"
+
+
+void loadPolygonSoup_OBJ(std::string filename, std::vector<std::array<double, 3>>& vertexPositionsOut,
+                  std::vector<std::vector<size_t>>& faceIndicesOut);
+
+void loadPolygonSoup_PLY(std::string filename, std::vector<std::array<double, 3>>& vertexPositionsOut,
+                  std::vector<std::vector<size_t>>& faceIndicesOut);
+
+// Load a mesh from a general file, detecting type from filename
+void loadPolygonSoup(std::string filename, std::vector<std::array<double, 3>>& vertexPositionsOut,
+              std::vector<std::vector<size_t>>& faceIndicesOut);
+
