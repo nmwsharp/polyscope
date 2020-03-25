fd31423dd48b1165a9a522c29a97c514b434fdc7
Nicholas Sharp <nsharp@cs.cmu.edu>
2020-03-25 01:44:15 -0400

    request 3.3 profile

--- src/render/opengl/gl_engine.cpp
+++ src/render/opengl/gl_engine.cpp
@@ -1597,8 +1597,8 @@ void GLEngine::initialize() {
   }
 
   // OpenGL version things
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 #if __APPLE__
   glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
