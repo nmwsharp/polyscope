c60079ecf529b95b8f9b6273b8b1b98926f340b1
Nicholas Sharp <nsharp@cs.cmu.edu>
2021-07-29 13:12:30 -0400

    show widget by default

--- src/structure.cpp
+++ src/structure.cpp
@@ -84,7 +84,7 @@ void Structure::buildUI() {
           // if there are none, show a helpful message
           if (ImGui::Button("Add slice plane")) {
             openSlicePlaneMenu = true;
-            addSceneSlicePlane();
+            addSceneSlicePlane(true);
           }
         } else {
           // otherwise, show toggles for each
