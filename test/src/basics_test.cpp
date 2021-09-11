2ba241b65b62bcbf09de674e2fc3017ab1cc92c0
Nicholas Sharp <nsharp@cs.cmu.edu>
2021-09-11 15:58:34 -0400

    add tests

--- test/src/basics_test.cpp
+++ test/src/basics_test.cpp
@@ -84,7 +84,9 @@ std::vector<glm::vec3> getPoints() {
 
 polyscope::PointCloud* registerPointCloud(std::string name = "test1") {
   std::vector<glm::vec3> points = getPoints();
-  return polyscope::registerPointCloud(name, points);
+  polyscope::PointCloud* psPoints = polyscope::registerPointCloud(name, points);
+  psPoints->setPointRenderMode(polyscope::PointRenderMode::Sphere);
+  return psPoints;
 }
 
 
@@ -109,6 +111,9 @@ TEST_F(PolyscopeTest, PointCloudAppearance) {
   psPoints->setMaterial("wax");
   EXPECT_EQ(psPoints->getMaterial(), "wax");
   polyscope::show(3);
+  
+  psPoints->setPointRenderMode(polyscope::PointRenderMode::Square);
+  polyscope::show(3);
 
   polyscope::removeAllStructures();
 }
@@ -117,6 +122,9 @@ TEST_F(PolyscopeTest, PointCloudPick) {
   auto psPoints = registerPointCloud();
 
   // Don't bother trying to actually click on anything, but make sure this doesn't crash
+  polyscope::pick::evaluatePickQuery(77, 88);
+  
+  psPoints->setPointRenderMode(polyscope::PointRenderMode::Square);
   polyscope::pick::evaluatePickQuery(77, 88);
 
   polyscope::removeAllStructures();
@@ -129,19 +137,8 @@ TEST_F(PolyscopeTest, PointCloudColor) {
   auto q1 = psPoints->addColorQuantity("vcolor", vColors);
   q1->setEnabled(true);
   polyscope::show(3);
-  polyscope::removeAllStructures();
-}
-
-TEST_F(PolyscopeTest, PointCloudParam) {
-  auto psPoints = registerPointCloud();
-  std::vector<glm::vec2> param(psPoints->nPoints(), glm::vec2{.2, .3});
-
-  auto q1 = psPoints->addParameterizationQuantity("param", param);
-  q1->setEnabled(true);
-  polyscope::show(3);
-
-  auto q2 = psPoints->addLocalParameterizationQuantity("local param", param);
-  q2->setEnabled(true);
+  
+  psPoints->setPointRenderMode(polyscope::PointRenderMode::Square);
   polyscope::show(3);
 
   polyscope::removeAllStructures();
@@ -152,6 +149,8 @@ TEST_F(PolyscopeTest, PointCloudScalar) {
   std::vector<double> vScalar(psPoints->nPoints(), 7.);
   auto q1 = psPoints->addScalarQuantity("vScalar", vScalar);
   q1->setEnabled(true);
+  polyscope::show(3);
+  psPoints->setPointRenderMode(polyscope::PointRenderMode::Square);
   polyscope::show(3);
   polyscope::removeAllStructures();
 }
@@ -166,6 +165,28 @@ TEST_F(PolyscopeTest, PointCloudVector) {
 }
 
 
+TEST_F(PolyscopeTest, PointCloudParam) {
+  auto psPoints = registerPointCloud();
+  std::vector<glm::vec2> param(psPoints->nPoints(), glm::vec2{.2, .3});
+
+  auto q1 = psPoints->addParameterizationQuantity("param", param);
+  q1->setEnabled(true);
+  polyscope::show(3);
+  
+  psPoints->setPointRenderMode(polyscope::PointRenderMode::Square);
+  polyscope::show(3);
+
+  auto q2 = psPoints->addLocalParameterizationQuantity("local param", param);
+  psPoints->setPointRenderMode(polyscope::PointRenderMode::Sphere);
+  q2->setEnabled(true);
+  polyscope::show(3);
+  
+  psPoints->setPointRenderMode(polyscope::PointRenderMode::Square);
+  polyscope::show(3);
+
+  polyscope::removeAllStructures();
+}
+
 TEST_F(PolyscopeTest, PointCloudScalarRadius) {
   auto psPoints = registerPointCloud();
   std::vector<double> vScalar(psPoints->nPoints(), 7.);
@@ -175,6 +196,9 @@ TEST_F(PolyscopeTest, PointCloudScalarRadius) {
   q1->setEnabled(true);
 
   psPoints->setPointRadiusQuantity(q1);
+  polyscope::show(3);
+  
+  psPoints->setPointRenderMode(polyscope::PointRenderMode::Square);
   polyscope::show(3);
 
   psPoints->setPointRadiusQuantity("vScalar2");
