d1eeaaaf60c6ba14cbb721af269045b8b5d8dec8
Nicholas Sharp <nsharp@cs.cmu.edu>
2022-05-27 15:54:06 -0400

    minor header update

--- include/polyscope/image_quantity.h
+++ include/polyscope/image_quantity.h
@@ -3,5 +3,10 @@
 // This is just a convenience header for users to get at the following functionality
 
 #include "polyscope/floating_quantity_structure.h"
-#include "polyscope/floating_scalar_image.h"
-#include "polyscope/floating_color_image.h"
+
+#include "polyscope/color_image_quantity.h"
+#include "polyscope/scalar_image_quantity.h"
+
+#include "polyscope/color_render_image_quantity.h"
+#include "polyscope/depth_render_image_quantity.h"
+#include "polyscope/scalar_render_image_quantity.h"
