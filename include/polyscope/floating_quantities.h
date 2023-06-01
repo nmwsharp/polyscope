825dd9904b4bf4733a33188c50af2237e1625bc6
Nicholas Sharp <nmwsharp@gmail.com>
2023-05-31 22:15:48 -0700

    add catchall header for floating quantities

---
+++ include/polyscope/floating_quantities.h
@@ -0,0 +1,10 @@
+// Copyright 2017-2023, Nicholas Sharp and the Polyscope contributors. https://polyscope.run
+
+#pragma once
+
+// A catch-all header for floating quantity includes
+
+#include "polyscope/color_image_quantity.h"
+#include "polyscope/floating_quantity.h"
+#include "polyscope/floating_quantity_structure.h"
+#include "polyscope/scalar_image_quantity.h"
