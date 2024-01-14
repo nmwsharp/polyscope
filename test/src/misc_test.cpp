acc6afca94b1fd27b39bef67daa881ab53cb2a3f
Nicholas Sharp <nmwsharp@gmail.com>
2024-01-14 01:06:58 -0800

    make scalar minmax limits persistent (#250)

--- test/src/misc_test.cpp
+++ test/src/misc_test.cpp
@@ -3,6 +3,31 @@
 #include "polyscope_test.h"
 
 // ============================================================
+// =============== Scalar Quantity Tests
+// ============================================================
+
+// We test these on a point cloud because it is convenient, but really we are testing the scalar quantity
+
+TEST_F(PolyscopeTest, TestScalarQuantity) {
+  auto psPoints = registerPointCloud();
+
+  std::vector<double> vScalar(psPoints->nPoints(), 7.);
+  auto q1 = psPoints->addScalarQuantity("vScalar", vScalar);
+  q1->setEnabled(true);
+  polyscope::show(3);
+
+  // get map range
+  std::pair<double, double> newRange = {-1., 1.};
+  q1->setMapRange(newRange);
+  EXPECT_EQ(newRange, q1->getMapRange());
+
+
+  polyscope::show(3);
+
+  polyscope::removeAllStructures();
+}
+
+// ============================================================
 // =============== Materials tests
 // ============================================================
 
