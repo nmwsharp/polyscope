af73a414e108ebdca6083a4ee7c37b6f221209d1
Nicholas Sharp <nmwsharp@gmail.com>
2024-03-03 10:13:14 -0800

    add missing include for root-level floating image quantities

--- include/polyscope/image_quantity.h
+++ include/polyscope/image_quantity.h
@@ -10,6 +10,7 @@
 #include "polyscope/color_image_quantity.h"
 #include "polyscope/color_render_image_quantity.h"
 #include "polyscope/depth_render_image_quantity.h"
+#include "polyscope/floating_quantity_structure.h"
 #include "polyscope/raw_color_alpha_render_image_quantity.h"
 #include "polyscope/raw_color_render_image_quantity.h"
 #include "polyscope/scalar_image_quantity.h"
