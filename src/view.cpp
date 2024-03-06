201da65739ec9b692e52d3c719b83d5205f01fcc
Jérémie Dumas <jeremie.dumas@ens-lyon.org>
2024-03-06 14:56:43 -0800

    Disable obsolete ImGui function + use IsKeyDown function.

--- src/view.cpp
+++ src/view.cpp
@@ -278,12 +278,12 @@ void processKeyboardNavigation(ImGuiIO& io) {
 
     glm::vec3 delta{0.f, 0.f, 0.f};
 
-    if (io.KeysDown[render::engine->getKeyCode('a')]) delta.x += 1.f;
-    if (io.KeysDown[render::engine->getKeyCode('d')]) delta.x += -1.f;
-    if (io.KeysDown[render::engine->getKeyCode('q')]) delta.y += 1.f;
-    if (io.KeysDown[render::engine->getKeyCode('e')]) delta.y += -1.f;
-    if (io.KeysDown[render::engine->getKeyCode('w')]) delta.z += 1.f;
-    if (io.KeysDown[render::engine->getKeyCode('s')]) delta.z += -1.f;
+    if (ImGui::IsKeyDown(static_cast<ImGuiKey>(render::engine->getKeyCode('a')))) delta.x += 1.f;
+    if (ImGui::IsKeyDown(static_cast<ImGuiKey>(render::engine->getKeyCode('d')))) delta.x += -1.f;
+    if (ImGui::IsKeyDown(static_cast<ImGuiKey>(render::engine->getKeyCode('q')))) delta.y += 1.f;
+    if (ImGui::IsKeyDown(static_cast<ImGuiKey>(render::engine->getKeyCode('e')))) delta.y += -1.f;
+    if (ImGui::IsKeyDown(static_cast<ImGuiKey>(render::engine->getKeyCode('w')))) delta.z += 1.f;
+    if (ImGui::IsKeyDown(static_cast<ImGuiKey>(render::engine->getKeyCode('s')))) delta.z += -1.f;
 
     if (glm::length(delta) > 0.) {
       hasMovement = true;
