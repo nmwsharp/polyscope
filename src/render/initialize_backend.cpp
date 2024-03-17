787ee84c0158d40dee9a81e04a4e06ac2ba122d9
Nicholas Sharp <nmwsharp@gmail.com>
2024-03-17 00:45:48 -0700

    update init code for new auto policy

--- src/render/initialize_backend.cpp
+++ src/render/initialize_backend.cpp
@@ -1,7 +1,10 @@
 // Copyright 2017-2023, Nicholas Sharp and the Polyscope contributors. https://polyscope.run
 
 #include "polyscope/messages.h"
+#include "polyscope/options.h"
 #include "polyscope/render/engine.h"
+
+#include <string>
 
 namespace polyscope {
 namespace render {
@@ -25,21 +28,8 @@ void initializeRenderEngine();
 void initializeRenderEngine(std::string backend) {
 
   // Handle default backends
-  // (the string is getting overwritten, so lower on the list means higher priority)
   if (backend == "") {
-
-#ifdef POLYSCOPE_BACKEND_OPENGL_MOCK_ENABLED
-    // Don't set it one by default, since it's probably a mistake; better to throw the exception below.
-    // backend = "mock_openGL";
-#endif
-
-#ifdef POLYSCOPE_BACKEND_OPENGL3_GLFW_ENABLED
-    backend = "openGL3_glfw";
-#endif
-
-    if (backend == "") {
-      exception("no Polyscope backends available");
-    }
+    backend = "auto"; // treat "" as "auto"
   }
 
   engineBackendName = backend;
@@ -51,6 +41,54 @@ void initializeRenderEngine(std::string backend) {
     backend_openGL3::initializeRenderEngine_egl();
   } else if (backend == "openGL_mock") {
     backend_openGL_mock::initializeRenderEngine();
+  } else if (backend == "auto") {
+
+    // Attempt to automatically initialize by trynig
+
+    bool initSucces = false;
+
+#ifdef POLYSCOPE_BACKEND_OPENGL3_GLFW_ENABLED
+    // First try GLFW, if available
+    backend = "openGL3_glfw";
+    try {
+      backend_openGL3::initializeRenderEngine_glfw();
+      initSucces = true;
+    } catch (const std::exception& e) {
+      if (options::verbosity > 0) {
+        info("Attempting automatic initialization. Could not initialize backend [openGL3_glfw]. Message: " +
+             std::string(e.what()));
+      }
+    }
+    if (initSucces) return;
+#endif
+
+#ifdef POLYSCOPE_BACKEND_OPENGL3_EGL_ENABLED
+    // Then, try EGL if available
+    backend = "openGL3_egl";
+    try {
+      backend_openGL3::initializeRenderEngine_egl();
+      initSucces = true;
+    } catch (const std::exception& e) {
+      if (options::verbosity > 0) {
+        info("Attempting automatic initialization. Could not initialize backend [openGL3_egl]. Message: " + e.what());
+      }
+    }
+    if (initSucces) {
+      if (options::verbosity > 0) {
+        info("Automatic initialization could not create an interactive backend, and created a headless backend "
+             "instead. This likely means no displays are available. With the headless backend, you can still run "
+             "Polyscope and even render, for instance to record screenshots. However no interactive windows can be "
+             "created.")
+      }
+      return;
+    }
+#endif
+
+    // Don't bother trying the 'mock' backend, it is unlikely to be what the user wants from the 'auto' option
+
+    // Failure
+    exception("Automatic initialization: no Polyscope backends could be initialized successfully.");
+
   } else {
     exception("unrecognized Polyscope backend " + backend);
   }
